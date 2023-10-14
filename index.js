653029a2cc5366593daf1551e3cb28753bec2957
EA-Gadgeter <emiacevedoposos@gmail.com>
2023-10-14 00:26:01 -0600

    Separado en 3 contenedores la app

---
+++ index.js
@@ -0,0 +1,27 @@
+const TODO_LIST = [
+  {
+    id: 1,
+    completed: true,
+    taskName: "Crear la base de datos"
+  },
+  {
+    id: 2,
+    completed: false,
+    taskName: "Programar la interfaz"
+  },
+  {
+    id: 3,
+    completed: false,
+    taskName: "Hacer del deploy"
+  },
+  {
+    id: 4,
+    completed: false,
+    taskName: "Recoger la ropa"
+  },
+  {
+    id: 5,
+    completed: false,
+    taskName: "Terminar la tarea"
+  }
+]
